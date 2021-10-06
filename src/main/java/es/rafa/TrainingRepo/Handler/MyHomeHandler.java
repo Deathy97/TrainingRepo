bde2c89e69617c6910250a5a5feee014f5385a20
Deathy97 <rafatorrea@gmail.com>
2021-10-06 16:16:53 +0200

    Add HTML view, not proud of it

--- src/main/java/es/rafa/TrainingRepo/Handler/MyHomeHandler.java
+++ src/main/java/es/rafa/TrainingRepo/Handler/MyHomeHandler.java
@@ -25,11 +25,12 @@ public class MyHomeHandler implements HttpHandler {
 	final static Logger logger = Logger.getLogger(MyHomeHandler.class);
 
 	public void handle(HttpExchange exchange) throws IOException {
-		List<UserBean> response = null;
-		String temporalResponse = "Hola Mundo";
-		
+		List<UserBean> userList = null;
+		String response = null;
+
 		try {
-			response = getUsers();
+			userList = getUsers();
+			response = buildResponse(userList);
 		} catch (IOException e) {
 			logger.error("Error while geting response", e);
 		} catch (InterruptedException e) {
@@ -37,11 +38,35 @@ public class MyHomeHandler implements HttpHandler {
 		} catch (ExecutionException e) {
 			logger.error("Error while making the request", e);
 		}
-		exchange.sendResponseHeaders(200, temporalResponse.length());
+		exchange.sendResponseHeaders(200, response.length());
 
 		OutputStream os = exchange.getResponseBody();
-		os.write(temporalResponse.getBytes());
+		os.write(response.getBytes());
 		os.close();
+	}
+
+	private String buildResponse(List<UserBean> userList) {
+		// NOT best way
+		String header = "<html><body>";
+		String footer = "</body></html>";
+		String tableHead = " <table><tr><th>ID</th><th>Login</th><th>Image</th>";
+		String tableFooter = "</tr></table>";
+
+		StringBuilder body = new StringBuilder();
+		body.append(tableHead);
+		userList.stream()
+				.forEach(user -> body.append("<tr><td style=' border: 1px solid black;'>" + user.getId()
+						+ "</td><td style=' border: 1px solid black;'>" + user.getLogin()
+						+ "</td><td style=' border: 1px solid black;'><img src='" + user.getAvatar_url()
+						+ "' width='80'></td></tr>"));
+		body.append(tableFooter);
+
+		StringBuilder result = new StringBuilder();
+		result.append(header);
+		result.append(body);
+		result.append(footer);
+
+		return result.toString();
 	}
 
 	private List<UserBean> getUsers() throws IOException, InterruptedException, ExecutionException {
@@ -52,7 +77,8 @@ public class MyHomeHandler implements HttpHandler {
 
 		CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,
 				HttpResponse.BodyHandlers.ofString());
-		List<UserBean> userList = mapper.readValue(response.get().body(), new TypeReference<List<UserBean>>(){});
+		List<UserBean> userList = mapper.readValue(response.get().body(), new TypeReference<List<UserBean>>() {
+		});
 
 		return userList;
 	}
