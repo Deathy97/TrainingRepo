b857321e18156e288e61fb93424e2c71c35746f2
Deathy97 <rafatorrea@gmail.com>
2021-10-06 13:54:38 +0200

    add Jackson and cast response list

--- src/main/java/es/rafa/TrainingRepo/Handler/MyHomeHandler.java
+++ src/main/java/es/rafa/TrainingRepo/Handler/MyHomeHandler.java
@@ -7,20 +7,27 @@ import java.net.http.HttpClient;
 import java.net.http.HttpClient.Version;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.log4j.Logger;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
+import es.rafa.TrainingRepo.bean.UserBean;
 
 public class MyHomeHandler implements HttpHandler {
 
 	final static Logger logger = Logger.getLogger(MyHomeHandler.class);
 
 	public void handle(HttpExchange exchange) throws IOException {
-		String response = null;
+		List<UserBean> response = null;
+		String temporalResponse = "Hola Mundo";
+		
 		try {
 			response = getUsers();
 		} catch (IOException e) {
@@ -30,22 +37,24 @@ public class MyHomeHandler implements HttpHandler {
 		} catch (ExecutionException e) {
 			logger.error("Error while making the request", e);
 		}
-		exchange.sendResponseHeaders(200, response.length());
+		exchange.sendResponseHeaders(200, temporalResponse.length());
 
 		OutputStream os = exchange.getResponseBody();
-		os.write(response.getBytes());
+		os.write(temporalResponse.getBytes());
 		os.close();
 	}
 
-	private String getUsers() throws IOException, InterruptedException, ExecutionException {
+	private List<UserBean> getUsers() throws IOException, InterruptedException, ExecutionException {
+		ObjectMapper mapper = new ObjectMapper();
 		HttpClient client = HttpClient.newBuilder().version(Version.HTTP_2).build();
 
 		HttpRequest request = HttpRequest.newBuilder(URI.create("https://api.github.com/users")).GET().build();
 
 		CompletableFuture<HttpResponse<String>> response = client.sendAsync(request,
 				HttpResponse.BodyHandlers.ofString());
+		List<UserBean> userList = mapper.readValue(response.get().body(), new TypeReference<List<UserBean>>(){});
 
-		return response.get().body().toString();
+		return userList;
 	}
 
 }
